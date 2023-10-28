756f96d618f2d8b2c70cafbe9033b17985e074b0
lh <pairofkings30@gmail.com>
2023-10-28 17:19:03 -0500

    fix conv2d

--- libs/model/templates/conv2d.py
+++ libs/model/templates/conv2d.py
@@ -3,7 +3,6 @@ import numpy as np
 from libs.model.nodetemplate import NodeTemplate
 from libs.model_helpers import activators, linalg
 
-
 class Conv2D(NodeTemplate):
     def __init__(self, F, K=1, P=0, flatten_output=False, **kwargs):
         self.layer_name = 'conv2d'
@@ -28,22 +27,23 @@ class Conv2D(NodeTemplate):
         for i in range(kernel_count):
             filter = kernel[:, :, :, i]
             result = linalg.convolve(xPadded, filter)
+            result = result + b[i]
             z[:, :, i] = linalg.reduce_shape(result)
 
         if self.flatten_output is True:
             z = z.flatten()
 
-        return z + b
+        return z
 
     def from_upstream(self, upstream: np.ndarray, x: np.ndarray, kernel: np.ndarray, b: np.ndarray):
         P = self.P
 
+        # Pad along second axis
         xPadded = linalg.pad_to_axis(x, P, axis=1)
-        xPadded = linalg.assert_shape(xPadded)
+        xPadded = linalg.assert_shape(xPadded) # To 3D
 
-        z = self.z(x, kernel, b)
+        z = self.z(x, kernel, b) # conv(x) + b
         z = z.flatten() if self.flatten_output is True else z
-        z += b
 
         da_dz = activators.drelu(z)
         dc_da_dz = upstream * da_dz
@@ -53,21 +53,21 @@ class Conv2D(NodeTemplate):
         dc_daL = np.zeros(x.shape)
         dc_dw = np.zeros(kernel.shape)
         depth = kernel.shape[2]
-        for i in range(depth):
+        for i in range(self.K):
             dc_da_dz_slice = linalg.assert_shape(dc_da_dz[:, :, i])
             result = linalg.convolve(xPadded, dc_da_dz_slice)
             dc_dw[:, :, :, i] = result
 
             filter = kernel[:, :, :, i]
             flipped_filter = np.flip(filter)
-            result = linalg.full_convolve(flipped_filter, dc_da_dz_slice)
+            result = linalg.full_convolve(dc_da_dz_slice, flipped_filter)
             result = result[P:-P, P:-P]  # Remove padding
-            result = result[:, :, 0]
+            # result = result[:, :, 0]
             result = linalg.match_shape(result, dc_daL)
             dc_daL += result
 
-        dc_da_dz = dc_da_dz.flatten() if self.flatten_output is True else dc_da_dz  # Correct shape
-
+        # dc_da_dz = dc_da_dz.flatten() if self.flatten_output is True else dc_da_dz  # Correct shape
+        dc_da_dz = np.einsum('ijk -> k', dc_da_dz)
         return dc_dw, dc_da_dz, dc_daL
 
 
@@ -91,3 +91,8 @@ class Conv2D(NodeTemplate):
         shape = (N, N) + (self.K,)
 
         return shape
+
+    def b_shape(self, input_shape=None) -> tuple:
+        input_shape = input_shape or self.input_shape
+
+        return (self.K,)
